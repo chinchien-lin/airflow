daf79ae0bf3eac4ba1661752003f867112c9eac6
Ash Berlin-Taylor <ash@apache.org>
2025-02-20 15:33:12 +0000

    Add JWT validation and generation machinery for the Task Execution API to use
    Since requests to Task Execution API can originate  out-of-"cluser" so to
    speak, this PR re-works the JWTSigner class so that it is possible to use
    public/private keys (vs just a simple pre-shared secret). This is useful in
    many cloud environments where many companies have security requirements that
    ingress gateways must validate the JWT tokens on the way in, and the only way
    of doing this is with public keys
    
    So that we don't have two different ways of generating JWT tokens I have
    totally replaced the old "JWTSigner" class (which it turns out didn't have any
    unit test of its own, it was only tested indirectly through test_serve_logs
    etc).
    
    As part of this change I have also changed the JWT that was generated by the
    SimpleAuthManager and the AwsAuthManager (the only two we have that use JWT)
    to use the offical `sub` (subject) clain to place the user identifer rather
    than a custom claim name.
    
    And although it might seem slightly strange at first, I have made the
    JWTValidator an async class internally. (Hence `avalidated_claims` -- the `a`
    prefix signifies async, much like `aclose` or `aread` on HTTPX async
    responses). This allows us to periodically refresh the JWK document if
    configured in the background, and using asgiref's async_to_sync means we only
    have one version.
    
    And also conviently, this is the same tech that FastAPI uses, which means when
    this is called from within a FastAPI app, rather than creating a new/nested
    event loop it will "bubble out" to the main async loop that FastAPI is using.

--- airflow/api_fastapi/core_api/security.py
+++ airflow/api_fastapi/core_api/security.py
@@ -41,7 +41,6 @@ from airflow.api_fastapi.auth.managers.models.resource_details import (
 from airflow.api_fastapi.core_api.base import OrmClause
 from airflow.configuration import conf
 from airflow.models.dag import DagModel
-from airflow.utils.jwt_signer import JWTSigner, get_signing_key
 
 if TYPE_CHECKING:
     from sqlalchemy.sql import Select
@@ -49,15 +48,6 @@ if TYPE_CHECKING:
     from airflow.api_fastapi.auth.managers.base_auth_manager import BaseAuthManager, ResourceMethod
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@cache
-def get_signer() -> JWTSigner:
-    return JWTSigner(
-        secret_key=get_signing_key("api", "auth_jwt_secret"),
-        expiration_time_in_seconds=conf.getint("api", "auth_jwt_expiration_time"),
-        audience="front-apis",
-    )
 
 
 def get_user(token_str: Annotated[str, Depends(oauth2_scheme)]) -> BaseUser:
